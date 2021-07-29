12d36c867079ac2af5278c6fabe04700c0a0f1a8
Parrilla, Arvin John <arvin.john.parrilla@accenture.com>
2021-07-29 19:48:16 +0800

    updated

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('', function(req, res, next) {
+router.post('/', function(req, res, next) {
     res.send('Hello')
 });
 
